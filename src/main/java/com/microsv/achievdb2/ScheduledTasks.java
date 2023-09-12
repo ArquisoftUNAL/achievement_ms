6567062db648ce1520b5ec56c2767a235606f768
edgardanielgd <edgardanielgd123@gmail.com>
2023-09-11 20:41:58 -0500

    Added chron job

--- src/main/java/com/microsv/achievdb2/ScheduledTasks.java
+++ src/main/java/com/microsv/achievdb2/ScheduledTasks.java
@@ -33,6 +33,6 @@ public class ScheduledTasks {
 
     @Scheduled(fixedRate = 5000)
     public void reportCurrentTime() {
-        log.info("The time is now {}", dateFormat.format(new Date()));
+        System.out.println("The time is now " + dateFormat.format(new Date()));
     }
 }
